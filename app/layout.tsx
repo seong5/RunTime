7ce7dee5ce77c5121b9715c5a5adaf8d7a07ebe2
seong5 <greenbi0852@gmail.com>
2025-08-05 17:45:31 +0900

    ✨ feat: 메인페이지 구현에 필요한 Gnb,footer 컴포넌트 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Gnb from '@/components/common/Gnb'
+import Footer from '@/components/common/Footer'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,7 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Gnb />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
