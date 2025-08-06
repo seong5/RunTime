53973b3f8db2d6a9c03b484320896222ff9db818
seong5 <greenbi0852@gmail.com>
2025-08-06 16:13:49 +0900

    ✨ feat: Footer.tsx 기본 구성 Link 와 반응형 구현

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
-    <div className="flex flex-row gap-20 text-gray-500 bg-white border-t border-gray-300 text-[16px] justify-evenly p-10">
-      <div>
+    <footer className="flex flex-row gap-20 text-gray-500 bg-white border-t border-gray-300 text-[12px] md:text-[16px] justify-evenly p-5 botton-0">
+      <Link href="https://github.com/seong5/RunTime">@RunTime-2025</Link>
+      <Link href="https://github.com/seong5">@seong5</Link>
+      <div className="text-[10px] md:text-[14px]">
         <p>greenbi0852@gmail.com</p>
         <p>010-4784-3867</p>
       </div>
-      <button>@seong5</button>
-      <button>gitHub</button>
-    </div>
+    </footer>
   )
 }
