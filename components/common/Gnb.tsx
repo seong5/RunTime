2d13ccf4ccfe9ccb3755662cdef244760f34b8da
seong5 <greenbi0852@gmail.com>
2025-08-06 15:49:35 +0900

    ✨ feat: 네비게이션 바 카테고리 및 색상 설정

--- components/common/Gnb.tsx
+++ components/common/Gnb.tsx
@@ -1,7 +1,11 @@
 export default function Gnb() {
   return (
-    <div>
-      네비게이션 바<div className="bg-orange">버튼</div>
+    <div className="flex flex-row gap-10 bg-navy text-white rounded-[12px] px-10 py-8">
+      <p className="mr-10">RunTime 로고자리임</p>
+      <button>내 기록</button>
+      <button>오늘의 날씨</button>
+      <button>챌린지</button>
+      <button>플레이 리스트</button>
     </div>
   )
 }
