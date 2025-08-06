8989bce4dcab5843b42d72bb6211df424b3864cd
seong5 <greenbi0852@gmail.com>
2025-08-06 16:37:58 +0900

    🎨 design: 네비게이션 색상 변경

--- components/common/Gnb.tsx
+++ components/common/Gnb.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 export default function Gnb() {
   return (
     <div className="flex flex-row gap-10 bg-navy text-[12px] md:text-[16px] text-white rounded-[12px] px-5 py-4 md:px-10 md:py-8">
-      <p className="mr-10">RunTime 로고자리임</p>
+      <p className="mr-10">RunTime</p>
       <Link href="/stats">내 기록</Link>
       <Link href="/weather">오늘의 날씨</Link>
       <Link href="/challenges">챌린지</Link>
-      <Link href="/songrec">플레이 리스트</Link>
+      <Link href="/songrec">플레이리스트</Link>
     </div>
   )
 }
