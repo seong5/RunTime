c00f27b83cd95e53009a5e0e300e920888d49711
seong5 <greenbi0852@gmail.com>
2025-08-06 18:04:10 +0900

    🎨 design: 이미지 반응형 2차 수정

--- components/LandingImages.tsx
+++ components/LandingImages.tsx
@@ -2,36 +2,28 @@ import Image from 'next/image'
 
 export default function LandingImages() {
   return (
-    <div className="w-full my-20 flex flex-col gap-20 justify-center items-center px-20 md:px-10">
-      <div className="relative w-full h-[200px] md:h-[400px]">
-        <Image
-          src="/images/landing1.jpg"
-          alt="랜딩 이미지1"
-          fill
-          className="object-cover rounded-[16px]"
-        />
-        <div className="absolute inset-0 bg-gradient-to-r from-white/100 via-transparent to-white/100 pointer-events-none rounded-[16px] " />
-      </div>
-
-      <div className="relative w-full h-[200px] md:h-[400px]">
-        <Image
-          src="/images/landing2.jpg"
-          alt="랜딩 이미지2"
-          fill
-          className="object-cover rounded-[16px]"
-        />
-        <div className="absolute inset-0 bg-gradient-to-r from-white/100 via-transparent to-white/100 pointer-events-none rounded-[16px]" />
-      </div>
-
-      <div className="relative w-full h-[200px] md:h-[400px]">
-        <Image
-          src="/images/landing3.jpg"
-          alt="랜딩 이미지3"
-          fill
-          className="object-cover rounded-[16px]"
-        />
-        <div className="absolute inset-0 bg-gradient-to-r from-white/100 via-transparent to-white/100 pointer-events-none rounded-[16px]" />
-      </div>
+    <div className="w-full my-10 md:my-20 flex flex-col gap-10 md:gap-20 justify-center items-center px-20 md:px-10">
+      {[
+        { src: '/images/landing1.jpg', text: '누구나 쉽게 시작할 수 있어요' },
+        { src: '/images/landing3.jpg', text: '건강한 하루를 만들어봐요' },
+        { src: '/images/landing2.jpg', text: '지금이 바로 RunTime!' },
+      ].map((item, index) => (
+        <div key={index} className="relative w-full h-[200px] md:h-[400px]">
+          <Image
+            src={item.src}
+            alt={`랜딩 이미지${index + 1}`}
+            fill
+            className="object-cover rounded-[16px] select-none pointer-events-none"
+            draggable={false}
+          />
+          <div className="absolute md:top-1/2 top-1/3 translate-y-[60%] md:translate-x-[-20%] md:translate-y-[40%] inset-0 z-20 flex justify-center">
+            <h2 className="text-white text-[20px] md:text-[50px] font-bold text-center drop-shadow-lg">
+              {item.text}
+            </h2>
+          </div>
+          <div className="w-full absolute inset-0 bg-gradient-to-r from-white/100 via-transparent to-white/100 pointer-events-none rounded-[16px]" />
+        </div>
+      ))}
     </div>
   )
 }
--- components/common/Gnb.tsx
+++ components/common/Gnb.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Gnb() {
   return (
-    <div className="flex flex-row gap-4 md:gap-10 bg-navy text-[10px] md:text-[16px] text-white rounded-[12px] px-5 py-4 md:px-10 md:py-8">
+    <div className="flex flex-row gap-4 md:gap-10 bg-navy text-[10px] md:text-[16px] text-white px-5 py-4 md:px-10 md:py-8">
       <Link href="/" className="mr-3 md:mr-10">
         RunTime
       </Link>
