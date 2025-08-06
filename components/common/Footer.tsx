24d68474df1443f36da87413d41c8a025fa88ce2
seong5 <greenbi0852@gmail.com>
2025-08-06 16:00:57 +0900

    ✨ feat: cn 패키지 설치 및 Footer.tsx 내용 작성

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <div>푸터</div>
+  return (
+    <div className="flex flex-row gap-20 text-gray-500 bg-white border-t border-gray-300 text-[16px] justify-evenly p-10">
+      <div>
+        <p>greenbi0852@gmail.com</p>
+        <p>010-4784-3867</p>
+      </div>
+      <button>@seong5</button>
+      <button>gitHub</button>
+    </div>
+  )
 }
