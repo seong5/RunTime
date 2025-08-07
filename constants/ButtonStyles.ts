4f039053728e1960d6bd7cef84629a2ed135842d
seong5 <greenbi0852@gmail.com>
2025-08-07 13:10:25 +0900

    ✨ feat:  버튼 크기 반응형 디자인 적용

--- constants/ButtonStyles.ts
+++ constants/ButtonStyles.ts
@@ -9,10 +9,10 @@ export const BUTTON_VARIANTS: Record<buttonVariants, string> = {
 
 export type buttonSize = 'xs' | 'sm' | 'md' | 'lg'
 export const BUTTON_SIZES: Record<buttonSize, string> = {
-  xs: 'w-full max-w-50 h-20',
-  sm: 'w-full max-w-70 h-30',
-  md: 'w-full max-w-100 h-30',
-  lg: 'w-full max-w-140 h-40',
+  xs: 'w-[150px] md:w-[200px] h-[60px] md:h-[70px]',
+  sm: 'w-[170px] md:w-[220px] h-[60px] md:h-[80px]',
+  md: 'w-[200px] md:w-[250px] h-[60px] md:h-[90px]',
+  lg: 'w-[240px] md:w-[290px] h-[60px] md:h-[100px]',
 }
 
 export type buttonRadius = 'none' | 'sm' | 'md' | 'lg' | 'full'
@@ -28,6 +28,6 @@ export type buttonText = 'sm' | 'md' | 'lg' | 'xl'
 export const BUTTON_TEXT: Record<buttonText, string> = {
   sm: 'text-[16px]',
   md: 'text-[20px] font-semibold',
-  lg: 'text-[24px] font-bold',
+  lg: 'text-[16px] md:text-[24px] font-bold',
   xl: 'text-[28px] font-bold',
 }
