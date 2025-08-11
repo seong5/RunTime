4c187ed9fcfae3e93b13bafaba2275dad73d80ca
seong5 <greenbi0852@gmail.com>
2025-08-11 15:39:22 +0900

    ✨ feat: 필터에 따른 평균 데이터를 보여주는 컴포넌트 파일 생성 및 import 적용

---
+++ app/stats/_components/RunningAverage.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function RunningAverage() {
+  return <div>필터에 따른 데이터</div>
+}
--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 import PeriodFilter from './_components/PeriodFilter'
+import RunningAverage from './_components/RunningAverage'
 
 export default function Stats() {
   return (
     <section className="px-[20px] md:px-[30px]">
       <PeriodFilter onChange={val => console.log('선택', val)} />
-      <div>날짜</div>
-      <div>뛴거리</div>
+      <RunningAverage />
       <div>그래프</div>
       <div>최근활동</div>
     </section>
