ff0bb79f82384f20616d1b1782f503ea99c1e898
seong5 <greenbi0852@gmail.com>
2025-08-11 13:12:17 +0900

    ✨ feat: IconsMap.ts 파일 생성 기록페이지에 우선적으로 들어갈 아이콘들을 테스트겸 사용하기 위해 만들었습니다.

---
+++ constants/IconsMap.ts
@@ -0,0 +1,6 @@
+const ICONS_MAP = {
+  ChevronDown: () => import('@/assets/icons/chevron-down-icon.png'),
+  Back: () => import('@/assets/icons/back-icon.png'),
+  More: () => import('@/assets/icons/more-icon.png'),
+}
+export default ICONS_MAP
