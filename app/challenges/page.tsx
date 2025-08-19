be773c5d39f2795bc9f3b0982194446af704b496
seong5 <greenbi0852@gmail.com>
2025-08-19 17:59:03 +0900

    ♻️ refactor: flex를 grid 로 변경

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -3,7 +3,7 @@ import { challenges } from '@/mocks/challenges'
 
 export default function Challenges() {
   return (
-    <div className="flex flex-col gap-5 p-4">
+    <div className="grid md:grid-cols-2 gap-5 p-4">
       {challenges.map(challenge => (
         <ChallengesCard key={challenge.id} challenge={challenge} />
       ))}
