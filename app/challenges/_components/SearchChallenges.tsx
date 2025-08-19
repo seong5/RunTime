df29ec4c4ac137110b2b9e556202c87d6cabe86d
seong5 <greenbi0852@gmail.com>
2025-08-19 18:22:36 +0900

    ✨ feat: 검색버튼 추가

--- app/challenges/_components/SearchChallenges.tsx
+++ app/challenges/_components/SearchChallenges.tsx
@@ -1,11 +1,14 @@
 export default function SearchChallenges() {
   return (
-    <div className="m-4">
+    <div className="flex flex-row items-center gap-4 m-4">
       <input
-        type="input"
+        type="search"
         placeholder="검색할 챌린지를 입력해주세요."
         className="rounded-full w-full border border-orange p-4"
       />
+      <button className="bg-green w-[120px] p-4 text-white text-[20px] font-semibold rounded-[16px]">
+        조회
+      </button>
     </div>
   )
 }
