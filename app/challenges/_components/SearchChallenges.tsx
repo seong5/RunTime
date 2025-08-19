86c5c9ec3a905a76fd0e10d56859a2d89cbbdd7c
seong5 <greenbi0852@gmail.com>
2025-08-19 18:07:25 +0900

    ✨ feat: 챌린지 검색기능을 담당할 컴포넌트 파일 생성

---
+++ app/challenges/_components/SearchChallenges.tsx
@@ -0,0 +1,11 @@
+export default function SearchChallenges() {
+  return (
+    <div className="m-4">
+      <input
+        type="input"
+        placeholder="검색할 챌린지를 입력해주세요."
+        className="rounded-full w-full border border-orange p-4"
+      />
+    </div>
+  )
+}
--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -1,12 +1,16 @@
 import ChallengesCard from './_components/ChallengesCard'
+import SearchChallenges from './_components/SearchChallenges'
 import { challenges } from '@/mocks/challenges'
 
 export default function Challenges() {
   return (
-    <div className="grid md:grid-cols-2 gap-5 p-4">
-      {challenges.map(challenge => (
-        <ChallengesCard key={challenge.id} challenge={challenge} />
-      ))}
-    </div>
+    <>
+      <SearchChallenges />
+      <div className="grid md:grid-cols-2 gap-5 p-4">
+        {challenges.map(challenge => (
+          <ChallengesCard key={challenge.id} challenge={challenge} />
+        ))}
+      </div>
+    </>
   )
 }
