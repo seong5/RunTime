3da5705b75d5edb520f7d466a1287db697b55642
seong5 <greenbi0852@gmail.com>
2025-08-19 16:11:56 +0900

    ✨ feat: 챌린지카드 컴포넌트 구현 및 challenges/page.tsx 에 렌더링 확인

--- app/challenges/_components/ChallengesCard.tsx
+++ app/challenges/_components/ChallengesCard.tsx
@@ -1,22 +1,18 @@
-import { challenges } from '@/mocks/challenges'
+import type { Challenges } from '@/mocks/challenges'
 
-export default function ChallengesCard() {
+type Props = { challenge: Challenges }
+
+export default function ChallengeCard({ challenge }: Props) {
   return (
-    <section className="bg-orange p-4 m-4 rounded-[16px]">
-      {challenges.map(challenge => (
-        <div key={challenge.id} className="flex flex-row items-center gap-5 md:gap-10">
-          <div className="bg-white rounded-[16px] w-[100px] md:w-[180px] h-[100px] md:h-[180px]">
-            챌린지이미지
-          </div>
-          <div className="text-white">
-            <div className="text-[20px] md:text-[45px] font-bold my-2 md:my-4">
-              {challenge.name}
-            </div>
-            <div className="text-[15px] md:text-[25px] font-semibold">{challenge.distance}km</div>
-            <div className="text-[15px] md:text-[25px] font-semibold">{challenge.success}</div>
-          </div>
-        </div>
-      ))}
-    </section>
+    <div className="flex flex-row bg-orange rounded-[16px] items-center gap-5 md:gap-10">
+      <div className="bg-white rounded-[16px] w-[100px] md:w-[180px] h-[100px] md:h-[180px] flex items-center justify-center">
+        챌린지이미지
+      </div>
+      <div className="text-white">
+        <div className="text-[20px] md:text-[45px] font-bold my-1 md:my-2">{challenge.name}</div>
+        <div className="text-[15px] md:text-[25px] font-semibold">{challenge.distance}km</div>
+        <div className="text-[15px] md:text-[25px] font-semibold">{challenge.success}</div>
+      </div>
+    </div>
   )
 }
--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -1,9 +1,12 @@
 import ChallengesCard from './_components/ChallengesCard'
+import { challenges } from '@/mocks/challenges'
 
 export default function Challenges() {
   return (
-    <div>
-      <ChallengesCard />
+    <div className="flex flex-col gap-5 p-4">
+      {challenges.map(challenge => (
+        <ChallengesCard key={challenge.id} challenge={challenge} />
+      ))}
     </div>
   )
 }
