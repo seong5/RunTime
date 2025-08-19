f9359034a51881aa03d356d290f0b5920d2be6a7
seong5 <greenbi0852@gmail.com>
2025-08-19 15:21:31 +0900

    ✨ feat: 챌린지에 필요한 mockData 생성 추후에 추가될 수 있음.

--- app/challenges/_components/ChallengesCard.tsx
+++ app/challenges/_components/ChallengesCard.tsx
@@ -1,3 +1,12 @@
 export default function ChallengesCard() {
-  return <div>챌린지카드</div>
+  return (
+    <section>
+      <div>챌린지이름</div>
+      <div>
+        <div>챌린지이미지</div>
+        <div>챌린지예상거리</div>
+        <div>챌린지성공여부</div>
+      </div>
+    </section>
+  )
 }
---
+++ mocks/challenges.ts
@@ -0,0 +1,8 @@
+// 챌린지 페이지에서 사용될 mockData
+export type challenges = {
+  id: number
+  distance: number
+  success: string
+}
+
+export const challenges = [{ id: 1, distance: 8, success: 'yes' }]
