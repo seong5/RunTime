cf2f591086cf3753924696cfbc1169ee04ecef5a
seong5 <greenbi0852@gmail.com>
2025-08-19 16:03:24 +0900

    ✨ feat: mockData 추가

--- app/challenges/_components/ChallengesCard.tsx
+++ app/challenges/_components/ChallengesCard.tsx
@@ -5,15 +5,15 @@ export default function ChallengesCard() {
     <section className="bg-orange p-4 m-4 rounded-[16px]">
       {challenges.map(challenge => (
         <div key={challenge.id} className="flex flex-row items-center gap-5 md:gap-10">
-          <div className="bg-gray-300 rounded-[16px] w-[100px] md:w-[180px] h-[100px] md:h-[180px]">
+          <div className="bg-white rounded-[16px] w-[100px] md:w-[180px] h-[100px] md:h-[180px]">
             챌린지이미지
           </div>
           <div className="text-white">
             <div className="text-[20px] md:text-[45px] font-bold my-2 md:my-4">
               {challenge.name}
             </div>
-            <div className="text-[15px] md:text-[25px]">{challenge.distance}km</div>
-            <div className="text-[15px] md:text-[25px]">{challenge.success}</div>
+            <div className="text-[15px] md:text-[25px] font-semibold">{challenge.distance}km</div>
+            <div className="text-[15px] md:text-[25px] font-semibold">{challenge.success}</div>
           </div>
         </div>
       ))}
--- mocks/challenges.ts
+++ mocks/challenges.ts
@@ -7,5 +7,9 @@ export type Challenges = {
 }
 
 export const challenges: Challenges[] = [
-  { id: 1, name: '종로 댕댕이런', distance: 8, success: 'yes' },
+  { id: 1, name: '종로 댕댕이런', distance: 8, success: 'Clear' },
+  { id: 2, name: '고래런', distance: 12, success: 'Clear' },
+  { id: 3, name: '런닝맨', distance: 8.7, success: 'Clear' },
+  { id: 4, name: '해골런', distance: 15.6, success: 'Clear' },
+  { id: 5, name: '고구마런', distance: 6, success: 'Clear' },
 ]
